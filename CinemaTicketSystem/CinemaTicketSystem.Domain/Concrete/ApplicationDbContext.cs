cd579166a5b31d50b74175918080471e26d8e8d4
Frank Schalkwijk <frenkkieboy@hotmail.com>
2018-05-23 15:53:32 +0200

    Rename and fixes, but no migrations yet.

--- CinemaTicketSystem/CinemaTicketSystem.Domain/Concrete/ApplicationDbContext.cs
+++ CinemaTicketSystem/CinemaTicketSystem.Domain/Concrete/ApplicationDbContext.cs
@@ -8,21 +8,10 @@ namespace CinemaTicketSystem.Domain.Concrete
 {
     public class ApplicationDbContext : DbContext
     {
-        // Your context has been configured to use a 'SchoolDbContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // '_20180308001.Models.SchoolDbContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'SchoolDbContext' 
-        // connection string in the application configuration file.
         public ApplicationDbContext()
             : base("name=ApplicationDbContext")
         {
         }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
 
         public virtual DbSet<Location> Locations { get; set; }
         public virtual DbSet<Movie> Movies { get; set; }
