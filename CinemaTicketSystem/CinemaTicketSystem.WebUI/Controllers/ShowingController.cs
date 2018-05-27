f19eff431d72790385aa176db66d903e54d7708a
Frank Schalkwijk <frenkkieboy@hotmail.com>
2018-05-27 16:04:19 +0200

    WebUI Showings index

---
+++ CinemaTicketSystem/CinemaTicketSystem.WebUI/Controllers/ShowingController.cs
@@ -0,0 +1,26 @@
+﻿using CinemaTicketSystem.Domain.Abstract;
+using CinemaTicketSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ShowingController : Controller
+    {
+        private IRepository repo;
+
+        public ShowingController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Showing> showings = repo.Get<Showing>(s => s.Start >= DateTime.Now, q => q.OrderBy(s => s.Start));
+            return View(showings);
+        }
+    }
+}
