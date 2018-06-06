9db0aaa814929d7a8d498c0bdf7811f38162c6c8
DolanL <dylan_leijten@hotmail.com>
2018-06-06 22:12:18 +0200

    style edit

--- CinemaTicketSystem/CinemaTicketSystem.WebUI/Controllers/OrderController.cs
+++ CinemaTicketSystem/CinemaTicketSystem.WebUI/Controllers/OrderController.cs
@@ -161,7 +161,7 @@ namespace WebUI.Controllers
             {
                 MailMessage mail = new MailMessage();
                 mail.To.Add(new MailAddress(email));
-                mail.Body = String.Format($"Bedankt voor je bestelling! Uw ordernummer is: {order.OrderNumber}");
+                mail.Body = String.Format($"Bedankt voor uw bestelling! Uw ordernummer is: {order.Id}");
                 mail.IsBodyHtml = true;
                 mailer.Send(mail);
             }
