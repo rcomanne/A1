675d58dd6b38cad82b7d2bbfa266e86953a4eae1
DolanL <dylan_leijten@hotmail.com>
2018-06-07 00:28:58 +0200

    Added map to cinema

---
+++ CinemaTicketSystem/CinemaTicketSystem.WebUI/Views/MapController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CinemaTicketSystem.Domain.Concrete;
+using CinemaTicketSystem.Domain.Entities;
+
+namespace WebUI.Views
+{
+    public class MapController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View(db.Locations.ToList());
+        }
+
+
+    }
+}
